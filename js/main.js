8afdef6463dd66b8f7d55cebe6713f2da3fe1794
Rafael Freire <rafaelfreireweb@gmail.com>
2021-07-06 14:16:21 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+  new ClipboardJS('.js-copy');
+
+  var SPMaskBehavior = function (val) {
+    return val.replace(/\D/g, '').length === 11 ? '(00) 00000.0000' : '(00) 0000.00009';
+  },
+  spOptions = {
+    onKeyPress: function(val, e, field, options) {
+        field.mask(SPMaskBehavior.apply({}, arguments), options);
+      }
+  };
+  $('.phone_mask').mask(SPMaskBehavior, spOptions);
+
+  $('.js-copy').click(function(){
+    var text =  $(this).find('.text');
+    text.text('Copiado!');
+    setTimeout(function(){
+      text.text('Copiar');
+    },5000);
+  });
+
+  $('.form-info input').each(function(){
+
+    $(this).keyup(function(e) {
+      var input = ".js-" + $(this).attr('name');
+      
+      if(e.target.value === "") {
+        $(input).text($(input).data('placeholder'));
+      } else {
+        if(input === ".js-phone1"){
+          $(input).text(" | " + e.target.value);
+        } else {
+          $(input).text(e.target.value);
+        }
+      }
+
+    });
+  });
+
+});
