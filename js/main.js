9fedecd9ec7aea9a4b0c272af87a767365723dac
Joel Cipriano <joelcipriano@users.noreply.github.com>
2024-06-11 09:19:20 -0300

    feat: remove o número fixo

--- js/main.js
+++ js/main.js
@@ -37,7 +37,7 @@ $(document).ready(function(){
         }
 
       } else {
-        if(input === ".js-phone1"){
+        if(input === ".js-phone2"){
           $(input).text(" | " + e.target.value);
         } else {
           $(input).text(e.target.value);
